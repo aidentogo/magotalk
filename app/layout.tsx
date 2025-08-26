c431a10232ad0dcdb982c876762a65bb2fd0583f
xiepeng <175533238xp@gmail.com>
2025-08-26 03:17:29 -0700

    chore-update-intro

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "MagoTalk - Web3 播客访谈系列",
-  description: "探索 Web3 世界的深度对话，与行业领袖一起探讨区块链、DeFi、NFT 等前沿话题",
+  title: "MagoTalk - 区块链与金融",
+  description: "探索 Web3 世界的深度对话，与行业嘉宾一起探讨区块链、DeFi、NFT 等前沿话题",
   keywords: ["Web3", "区块链", "播客", "访谈", "DeFi", "NFT", "元宇宙"],
   authors: [{ name: "MAGO" }],
   openGraph: {
